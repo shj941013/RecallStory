7579378693ee583107421c28477b1fea56b3b9a2
Hyunjae Song <song1003@yahoo.com>
2014-07-28 11:08:57 -0400

    fixed some stff

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -20,36 +20,30 @@ class MemoriesController < ApplicationController
   def create
     @memory = current_user.memories.build(memory_params)
 
-    respond_to do |format|
       if @memory.save
         format.html { redirect_to @memory, notice: 'Memory was successfully created.' }
       else
         format.html { render :new }
       end
-    end
   end
 
   # PATCH/PUT /memories/1
   # PATCH/PUT /memories/1.json
   def update
-    respond_to do |format|
+ 
       if @memory.update(memory_params)
-        format.html { redirect_to @memory, notice: 'Memory was successfully updated.' }
-        format.json { render :show, status: :ok, location: @memory }
+        redirect_to @memory, notice: 'Memory was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @memory.errors, status: :unprocessable_entity }
+        render :edit 
       end
-    end
   end
 
   def destroy
     @memory.destroy
-    respond_to do |format|
-      format.html { redirect_to memories_url, notice: 'Memory was successfully destroyed.' }
-      format.json { head :no_content }
+
+      redirect_to memories_url, notice: 'Memory was successfully destroyed.' 
+      head :no_content 
     end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
