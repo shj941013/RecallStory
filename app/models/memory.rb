64eac042dd06fad879810e589f0d87198f7775ce
Hyunjae Song <song1003@yahoo.com>
2014-07-31 12:50:19 -0400

    added validation for name changed name

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -2,4 +2,6 @@ class Memory < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+	validates :image, presence: true
 end
